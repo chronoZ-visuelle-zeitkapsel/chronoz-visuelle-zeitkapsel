7b03b9d1e5fcbda5a506079aaa5aaa07a35e1725
rapidler11 <200006@studierende.htl-donaustadt.at>
2025-12-12 08:59:36 +0100

    Replace bcrypt with bcryptjs for Vercel compatibility

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config();
